569e201de2291bf61a07deeb580331a8b3ba992b
diegonline86 <diegosilguero@yahoo.es>
2015-11-09 10:30:44 +0100

    ejercicio 1 completo

--- boletin8/com/salesianostriana/sge/boletin8/ejercicio1.py
+++ boletin8/com/salesianostriana/sge/boletin8/ejercicio1.py
@@ -1,14 +1,24 @@
 __author__ = 'Diego Alberto Silguero'
 # -*- coding: utf-8 -*-
+import operator
+
 votos = dict()
 nombre = ""
 
 while(nombre != "FIN"):
+    nombre = input("Introduce nombre: ")
     if(nombre != "FIN"):
-        nombre = input("Introduce nombre: ")
         cantidad = int(input("Cantidad de votos: "))
         votos[nombre]=cantidad
 
-print(votos)
+#mostramos una representacion del dictionary ordenado por valores de manera descendente
+print("\nResultados de la votación"
+      "\n------------------------")
+print(sorted(votos.items(), key = operator.itemgetter(1), reverse= True))
+#imprimimos al más votado
+print("\nGanador"
+      "\n---------")
+print(max(sorted(votos.items(), key = operator.itemgetter(1), reverse= True)))
+
 
 
